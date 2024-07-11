72cb21aa4f70441084a38248426425cb78c874c5
Prathmesh <prathmeshjain340@gmail.com>
2024-07-11 14:57:56 +0530

    lambda file is created

---
+++ LambdaFunctonExample.py
@@ -0,0 +1,20 @@
+# def double(x):
+#     return x*2
+# print(double(5))
+
+# double =lambda x:x*2
+# print(double(5))
+def apply(fx,value):
+    return 6+fx(value)
+
+
+cube= lambda i:i*i*i
+# print(cube(5))
+
+
+average= lambda  x,y:(x+y)/2
+print(average(3,5))
+
+print(apply(cube,2))
+
+    
